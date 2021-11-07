6f879903e7360edc56571f2a1804c2a1a891da7d
SilvenLEAF <SilvenLEAF@gmail.com>
2021-11-07 19:06:48 +0530

    about me updated

--- client/src/components/about/About.js
+++ client/src/components/about/About.js
@@ -6,11 +6,15 @@ import React from 'react'
 function About() {
   return (
     <div id="myAbout" className="container scrollspy">
-      
+
       <div className="myAboutInfoHolder">
 
         <p className="myAboutParagraph" >
-          Hi there! I'm SilvenLEAF and I love to code a lot. I love starting my day on 4.00am with coding, when the whole world is half asleep. I also love to learn Chinese and Japanese. I'm a big fan of tech and SUPER BIG FAN of Chinese Sweet FLUFFY ROMANCE DRAMAS.  I'm scared of snakes, but I LOVE animals a lot. I love listening to JPOP. Favorite singer Kenshi Younezu. Favorite color is Red. Favorite hobby Coding. Favorite song is "Peace Sign (Kenshi Younezu)".
+          Hi there! I'm SilvenLEAF and I'm insanely crazy, and madly in love with TypeScript and JavaScript. I'm a backend maniac, mainly Node based, but insanely hungry for anything programming related. I find peace amonst the insanest stuff! And that is BackEND! I also do some random Python stuff! Yes, I'm fullstack, but BackEND is where my soul resides! Thirsty for Programming, Hungry for Backend, and aspiring for DevOps!!
+        </p>
+
+        <p className="myAboutParagraph" >
+          I am also insanely in love with Chinese and Japanese. I'm a big fan of tech and ULTRA BIG FAN of Chinese Sweet FLUFFY ROMANCE DRAMAS. I'm an introvert. I love listening to JPOP. Favorite singer Kenshi Younezu. Favorite color is Red. Favorite hobby Programming.
         </p>
 
         <div className="myAboutQA">
@@ -39,7 +43,7 @@ function About() {
 
         <div className="myAboutQA">
           <div className="myAboutTitle">Age</div>
-          <div className="myAboutAnswer">{new Date().getFullYear() - 2001}</div>
+          <div className="myAboutAnswer">{new Date().getFullYear() - 2002}</div>
         </div>
 
 
@@ -49,10 +53,10 @@ function About() {
           <div className="myAboutTitle">Gender</div>
           <div className="myAboutAnswer">Male</div>
         </div>
-        
-        
-        
-        
+
+
+
+
         <div className="myAboutQA">
           <div className="myAboutTitle">Languages</div>
           <div className="myAboutAnswer">English, Chinese, Japanese, Hindi, Bengali</div>
@@ -60,14 +64,14 @@ function About() {
 
 
 
-        
+
         <div className="myAboutQA">
           <div className="myAboutTitle">Hobbies</div>
-          <div className="myAboutAnswer">Coding, Learning Chinese/Japanese, Creating, Problem Solving</div>
+          <div className="myAboutAnswer">Programming, Learning Chinese/Japanese, Creating, Problem Solving</div>
         </div>
-        
-        
-        
+
+
+
         <div className="myAboutQA">
           <div className="myAboutTitle">My Blogs</div>
           <div className="myAboutAnswer">
